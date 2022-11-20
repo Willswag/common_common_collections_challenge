d6c5139b6db3206e2b1ad923519651d33144a975
Spencer Waguespack <wswaguespack403@gmail.com>
2022-11-20 14:30:49 -0500

    problem statements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// given a list of ints use a vector and return the mean and mode
+// convert a string to pig latin 
+//  the first consonant goes to the end of the word and the "ay" is added to back
+
+
 fn main() {
     println!("Hello, world!");
 }
