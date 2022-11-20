f0558f188f7d6d082e53810f8eea5c44c4d365f2
Spencer Waguespack <wswaguespack403@gmail.com>
2022-11-20 14:30:59 -0500

    re doo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 // given a list of ints use a vector and return the mean and mode
+
 // convert a string to pig latin 
 //  the first consonant goes to the end of the word and the "ay" is added to back
+
+// hashmap and vector that allow a user to store names and departments
+
 
 
 fn main() {
